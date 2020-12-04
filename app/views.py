e05c1ea81090eae085b25b99ebcab2d7bbd81405
danielspring-crypto <borncode3@gmail.com>
2020-12-05 03:52:31 +0630

    first commit

---
+++ app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, "app/index.html")
+
+def one(request):
+	return render(request, "app/one.html")
+
+def two(request):
+	return render(request, "app/two.html")
+
+def three(request):
+	return render(request, "app/three.html")
+
+def four(request):
+	return render(request, "app/four.html")
+
+def five(request):
+	return render(request, "app/five.html")
+
+def six(request):
+	return render(request, "app/six.html")
+
+def seven(request):
+	return render(request, "app/seven.html")
+
+def eight(request):
+	return render(request, "app/eight.html")
+
+def nine(request):
+	return render(request, "app/nine.html")
+
+def ten(request):
+	return render(request, "app/ten.html")
